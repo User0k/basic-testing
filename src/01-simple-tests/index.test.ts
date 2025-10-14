c7470ad6822deca8bfb1b8565c733f725be83820
Pavel Altov <pavel.altov@yahoo.com>
2025-10-14 23:09:58 +0300

    feat: basic calculator tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,37 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
+  const calc = (
+    arg1: number | string,
+    arg2: number,
+    operand: Action | string,
+  ) => simpleCalculator({ a: arg1, b: arg2, action: operand });
+
   test('should add two numbers', () => {
-    // Write your test here
+    expect(calc(2, 3, Action.Add)).toBe(5);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(calc(2, 3, Action.Subtract)).toBe(-1);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(calc(2, 3, Action.Multiply)).toBe(6);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(calc(10, -2, Action.Divide)).toBe(-5);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(calc(2, 10, Action.Exponentiate)).toBe(1024);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(calc(2, 3, 'Nonsense')).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(calc('2', 3, Action.Multiply)).toBeNull();
   });
 });
